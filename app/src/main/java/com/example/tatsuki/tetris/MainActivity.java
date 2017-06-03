36db482473c8b7754698bc04f825ec066a8aadd7
Tatsuki Matsumoto <tatsukimatsumo@gmail.com>
2017-06-03 23:23:04 +0900

    Improve touch event

--- app/src/main/java/com/example/tatsuki/tetris/MainActivity.java
+++ app/src/main/java/com/example/tatsuki/tetris/MainActivity.java
@@ -12,16 +12,21 @@ import android.view.WindowManager;
  */
 public class MainActivity extends Activity {
 
-  @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    this.getWindow().clearFlags(
-            WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN
-    );
-    this.getWindow().addFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-    );
-    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-    setContentView(new MainView(this));
-  }
+    /**
+     * 画面表示時に実行
+     */
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.getWindow().clearFlags(
+                WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN
+        );
+        this.getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(new MainView(this));
+    }
 }
--- app/src/main/java/com/example/tatsuki/tetris/MainView.java
+++ app/src/main/java/com/example/tatsuki/tetris/MainView.java
@@ -15,91 +15,64 @@ import com.example.tatsuki.tetris.tetriminos.TetriminoGenerator;
 /**
  * Created by tatsuki on 2017/06/01.
  */
-
 public class MainView extends SurfaceView implements SurfaceHolder.Callback, Runnable, GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+    /**
+     * ジェスチャを検知
+     */
     private GestureDetector gestureDetector;
+    /**
+     * viewのホルダー
+     */
     private SurfaceHolder holder;
+    /**
+     * SurFaceView用のスレッド
+     */
     private Thread thread;
-
+    /**
+     * ゲーム中であるあどうか
+     */
     private static Boolean isGaming = true;
-    private static int input = 0;
-
-    // フィールド
+    /**
+     * フィールド
+     */
     private static Field field = new Field();
-    // テトリミノ
+    /**
+     * テトリミノ
+     */
     private static AbstractTetrimino tetrimino = TetriminoGenerator.generateTetrimino();
 
+    /**
+     * コンストラクタ
+     */
     public MainView(Context context) {
         super(context);
 
-        holder = this.getHolder();
-        holder.addCallback(this);
+        this.holder = this.getHolder();
+        this.holder.addCallback(this);
 
         this.gestureDetector = new GestureDetector(context, this);
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        // 何もしない
-    }
-
-    @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        thread = null;
+        this.thread = null;
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        thread = new Thread(this);
-        thread.start();
-    }
-
-    @Override
-    public boolean onDoubleTap(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onDoubleTapEvent(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent e) {
-
+        this.thread = new Thread(this);
+        this.thread.start();
     }
 
     public void move(int deltaX, int deltaY) {
         if (tetrimino.canMove(field.getField(), deltaX, deltaY)) {
             tetrimino.move(deltaX, deltaY);
-            this.printOut();
         }
     }
 
     public void turn(int direction) {
         if (tetrimino.canTurn(field.getField(), direction)) {
             tetrimino.turn(direction);
-            this.printOut();
         }
     }
 
@@ -107,11 +80,16 @@ public class MainView extends SurfaceView implements SurfaceHolder.Callback, Run
     public void run() {
         while (this.thread != null && !(field.isGameOver(tetrimino))) {
             long nextTime = System.currentTimeMillis() + 500;
+
             onTick();
+
             try {
-                Thread.sleep(nextTime - System.currentTimeMillis());
-            } catch (Exception e) {
-                // なにもできない
+                long sleepTime = nextTime - System.currentTimeMillis();
+                if (sleepTime >= 0) {
+                    Thread.sleep(sleepTime);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
 
@@ -138,6 +116,7 @@ public class MainView extends SurfaceView implements SurfaceHolder.Callback, Run
 
         canvas.drawColor(Color.WHITE);
         field.printOut(tetrimino, canvas);
+
         holder.unlockCanvasAndPost(canvas);
     }
 
@@ -148,7 +127,7 @@ public class MainView extends SurfaceView implements SurfaceHolder.Callback, Run
     }
 
     @Override
-    public boolean onSingleTapConfirmed(MotionEvent e) {
+    public boolean onSingleTapUp(MotionEvent e) {
         int width = this.getWidth();
 
         int deltaX;
@@ -159,19 +138,81 @@ public class MainView extends SurfaceView implements SurfaceHolder.Callback, Run
         }
 
         move(deltaX, 0);
+        this.printOut();
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        int width = this.getWidth();
+
+        int deltaX;
+        if (e.getX() < width / 2) {
+            deltaX = -1;
+        } else {
+            deltaX = 1;
+        }
+
+        move(deltaX, 0);
+        this.printOut();
         return false;
     }
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        int direction;
-        if (velocityX < 0) {
-            direction = -1;
+        if (Math.abs(velocityY) > Math.abs(velocityX)) {
+            // 下向きにフリックされたら落とす
+            while (this.tetrimino.canMove(this.field.getField(), 0, 1)) {
+                this.tetrimino.move(0, 1);
+            }
         } else {
-            direction = 1;
+            // 横向きにフリックされたら回転
+            int direction;
+            if (velocityX < 0) {
+                direction = -1;
+            } else {
+                direction = 1;
+            }
+
+            turn(direction);
         }
 
-        turn(direction);
+        this.printOut();
         return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
     }
 }
